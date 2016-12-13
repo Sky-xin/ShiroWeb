005c95d23efab80aebc124eb73f0dcb58767b815
Sky-xin <251062623@qq.com>
2016-12-13 09:58:03 +0800

    xml修改

--- src/main/java/com/vpcloud/shiro/controller/web/ViewController.java
+++ src/main/java/com/vpcloud/shiro/controller/web/ViewController.java
@@ -2,13 +2,22 @@ package com.vpcloud.shiro.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class ViewController {
 	
-	@RequestMapping("/")
+	@RequestMapping("login")
 	public String index(){
+		System.out.println(">>>>>>>>>>>>>>>>>>>");
 		return "index";
+	}
+	@RequestMapping("get")
+	@ResponseBody
+	public String getTest(){
+		System.out.println("?>>>>>>>>>>>>>>>");
+		return "hehe";
 	}
 
 }
---
+++ src/test/java/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		byte[] a = {59,0,0,1,-123,-90,-1,-1,0,0,0,1,28,0,0};
+		System.out.println(new String(a));
+	}
+}
